74be2aa5e6b3c929e60fb354bd9172b02d8858be
SamuelRahul <114229941+SamuelRahul@users.noreply.github.com>
2022-09-23 16:55:21 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer class="footer-area section-gap">
+        <div class="container">
+          
+            <div class="footer-bottom row align-items-center text-center text-lg-left">
+                
+            </div>
+        </div>
+    </footer>
